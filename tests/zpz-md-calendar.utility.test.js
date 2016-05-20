7a19d2c8f3616c25151f9cfc37f1fe2074382e87
Joseph Matthias Goh <joseph@zephinzer.com>
2016-05-21 04:34:32 +0800

    Added tests for zpzMdCalendarUtilitySvc.mergeRecursive()

--- tests/zpz-md-calendar.utility.test.js
+++ tests/zpz-md-calendar.utility.test.js
@@ -53,7 +53,9 @@ describe(
 			'returns number of days in the month',
 			/** ------------------------------------------------------------------ **/ 
 			inject(function(zpzMdCalendarUtilitySvc) {
-				zpzMdCalendarUtilitySvc.getNumberOfDaysInMonth(1, 2015).should.equal(28);
+				zpzMdCalendarUtilitySvc
+					.getNumberOfDaysInMonth(1, 2015)
+					.should.equal(28);
 			})
 		);
 		it(
@@ -61,11 +63,21 @@ describe(
 			'accounts for leap years properly',
 			/** ------------------------------------------------------------------ **/ 
 			inject(function(zpzMdCalendarUtilitySvc) {
-				zpzMdCalendarUtilitySvc.getNumberOfDaysInMonth(1, 2016).should.equal(29);
-				zpzMdCalendarUtilitySvc.getNumberOfDaysInMonth(1, 2017).should.equal(28);
-				zpzMdCalendarUtilitySvc.getNumberOfDaysInMonth(1, 2018).should.equal(28);
-				zpzMdCalendarUtilitySvc.getNumberOfDaysInMonth(1, 2019).should.equal(28);
-				zpzMdCalendarUtilitySvc.getNumberOfDaysInMonth(1, 2020).should.equal(29);
+				zpzMdCalendarUtilitySvc
+					.getNumberOfDaysInMonth(1, 2016)
+					.should.equal(29);
+				zpzMdCalendarUtilitySvc
+					.getNumberOfDaysInMonth(1, 2017)
+					.should.equal(28);
+				zpzMdCalendarUtilitySvc
+					.getNumberOfDaysInMonth(1, 2018)
+					.should.equal(28);
+				zpzMdCalendarUtilitySvc
+					.getNumberOfDaysInMonth(1, 2019)
+					.should.equal(28);
+				zpzMdCalendarUtilitySvc
+					.getNumberOfDaysInMonth(1, 2020)
+					.should.equal(29);
 			})
 		);
 	});
@@ -82,9 +94,216 @@ describe(
 			inject(function(zpzMdCalendarUtilitySvc) {
 				var may2016 = 0,
 					jun1983 = 3;
-				zpzMdCalendarUtilitySvc.getFirstDayOfMonth(4, 2016).should.equal(may2016);
-				zpzMdCalendarUtilitySvc.getFirstDayOfMonth(5, 1983).should.equal(jun1983);
+				zpzMdCalendarUtilitySvc
+					.getFirstDayOfMonth(4, 2016)
+					.should.equal(may2016);
+				zpzMdCalendarUtilitySvc
+					.getFirstDayOfMonth(5, 1983)
+					.should.equal(jun1983);
 			})
 		);
 	});
+	describe(
+		/** ----------------------------------------------------------------------- */
+		'mergeRecursive()',
+		/** ----------------------------------------------------------------------- */
+		function() {
+			/**
+			 * @var NUMBER : Test Number-typed rvalue : 1
+			 */
+			var NUMBER = 1;
+			/**
+			 * @var STRING : Test String-typed rvalue
+			 */
+			var STRING = 'a';
+			/**
+			 * @var dst : Test Object-typed lvalue
+			 */
+			var dst;
+			/**
+			 * Renew `dst` at every test case
+			 */
+			beforeEach(function() {
+				dst = {
+					a1: {
+						a2: {
+							a3: {
+								a4: NUMBER
+							},
+							b3: NUMBER,
+							c3: STRING
+						},
+						b2: NUMBER,
+						c2: STRING
+					},
+					b1: {
+						a2: NUMBER,
+						b2: STRING,
+						c2: {
+							a3: NUMBER,
+							b3: STRING,
+							c3: {
+								a4: NUMBER
+							}
+						}
+					},
+					c1: {
+						a2: STRING,
+						b2: {
+							a3: STRING,
+							b3: {
+								a4: NUMBER
+							},
+							c3: NUMBER
+						},
+						c2: NUMBER
+					},
+					d1: NUMBER,
+					e1: STRING
+				};
+			});
+			
+			it(
+				/** -------------------------------------------------------------- **/
+				'Source value replaces destination object at level 1',
+				/** -------------------------------------------------------------- **/
+				inject(function(zpzMdCalendarUtilitySvc) {
+					var src = { a1: 5 },
+						out = zpzMdCalendarUtilitySvc.mergeRecursive(dst, src);
+					out.a1.should.equal(5);
+				})
+			);
+			it(
+				/** -------------------------------------------------------------- **/
+				'Source value replaces destination value at level 1 (homotyped)',
+				/** -------------------------------------------------------------- **/
+				inject(function(zpzMdCalendarUtilitySvc) {
+					var src = { d1: 5 },
+						out = zpzMdCalendarUtilitySvc.mergeRecursive(dst, src);
+					out.d1.should.equal(5);
+					
+					src = { e1: 'b' };
+					out = zpzMdCalendarUtilitySvc.mergeRecursive(dst, src);
+					out.e1.should.equal('b');
+				})
+			);
+			it(
+				/** -------------------------------------------------------------- **/
+				'Source value replaces destination value at level 1 (heterotyped)',
+				/** -------------------------------------------------------------- **/
+				inject(function(zpzMdCalendarUtilitySvc) {
+					var src = { d1: 'b' },
+						out = zpzMdCalendarUtilitySvc.mergeRecursive(dst, src);
+					out.d1.should.equal('b');
+					
+					src = { e1: 5 };
+					out = zpzMdCalendarUtilitySvc.mergeRecursive(dst, src);
+					out.e1.should.equal(5);
+				})
+			);
+			it(
+				/** -------------------------------------------------------------- **/
+				'Source value replaces destination object at level 2',
+				/** -------------------------------------------------------------- **/
+				inject(function(zpzMdCalendarUtilitySvc) {
+					var src = { a1: { a2: 5} },
+						out = zpzMdCalendarUtilitySvc.mergeRecursive(dst, src);
+					out.a1.a2.should.equal(5);
+				})
+			);
+			it(
+				/** -------------------------------------------------------------- **/
+				'Source value replaces destination value at level 2 (homotyped)',
+				/** -------------------------------------------------------------- **/
+				inject(function(zpzMdCalendarUtilitySvc) {
+					var src = { a1: { b2: 5 } },
+						out = zpzMdCalendarUtilitySvc.mergeRecursive(dst, src);
+					out.a1.b2.should.equal(5);
+					
+					src = { a1: { c2: 'b' } };
+					out = zpzMdCalendarUtilitySvc.mergeRecursive(dst, src);
+					out.a1.c2.should.equal('b');
+				})
+			);
+			it(
+				/** -------------------------------------------------------------- **/
+				'Source value replaces destination value at level 2 (heterotyped)',
+				/** -------------------------------------------------------------- **/
+				inject(function(zpzMdCalendarUtilitySvc) {
+					var src = { a1: { b2: 'b' } },
+						out = zpzMdCalendarUtilitySvc.mergeRecursive(dst, src);
+					out.a1.b2.should.equal('b');
+					
+					src = { a1: { c2: 5 } };
+					out = zpzMdCalendarUtilitySvc.mergeRecursive(dst, src);
+					out.a1.c2.should.equal(5);
+				})
+			);
+			it(
+				/** -------------------------------------------------------------- **/
+				'Source value replaces destination object at level 3',
+				/** -------------------------------------------------------------- **/
+				inject(function(zpzMdCalendarUtilitySvc) {
+					var src = { a1: { a2: { a3: 5 } } },
+						out = zpzMdCalendarUtilitySvc.mergeRecursive(dst, src);
+					out.a1.a2.a3.should.equal(5);
+					
+				})
+			);
+			it(
+				/** -------------------------------------------------------------- **/
+				'Source value replaces destination value at level 3 (homotyped)',
+				/** -------------------------------------------------------------- **/
+				inject(function(zpzMdCalendarUtilitySvc) {
+					var src = { a1: { a2: { b3: 5 } } },
+						out = zpzMdCalendarUtilitySvc.mergeRecursive(dst, src);
+					out.a1.a2.b3.should.equal(5);
+					
+					src = { a1: { a2: { c3: 'b' } } };
+					out = zpzMdCalendarUtilitySvc.mergeRecursive(dst, src);
+					out.a1.a2.c3.should.equal('b');
+				})
+			);
+			it(
+				/** -------------------------------------------------------------- **/
+				'Source value replaces destination value at level 3 (heterotyped)',
+				/** -------------------------------------------------------------- **/
+				inject(function(zpzMdCalendarUtilitySvc) {
+					var src = { a1: { a2: { b3: 'b' } } },
+						out = zpzMdCalendarUtilitySvc.mergeRecursive(dst, src);
+					out.a1.a2.b3.should.equal('b');
+					
+					src = { a1: { a2: { c3: 5 } } };
+					out = zpzMdCalendarUtilitySvc.mergeRecursive(dst, src);
+					out.a1.a2.c3.should.equal(5);
+				})
+			);
+			it(
+				/** -------------------------------------------------------------- **/
+				'Works with more than one source value to replace',
+				/** -------------------------------------------------------------- **/
+				inject(function(zpzMdCalendarUtilitySvc) {
+					var src = { a1: { a2: { b3: 'b', c3: 2 } } },
+						out = zpzMdCalendarUtilitySvc.mergeRecursive(dst, src);
+					out.a1.a2.b3.should.equal('b');
+					out.a1.a2.c3.should.equal(2);
+					
+					src = { a1: { b2: 'b', c2: 2 } };
+					out = zpzMdCalendarUtilitySvc.mergeRecursive(dst, src);
+					out.a1.b2.should.equal('b');
+					out.a1.c2.should.equal(2);
+				})
+			);
+			it(
+				/** -------------------------------------------------------------- **/
+				'Works with more than one source object to replace',
+				/** -------------------------------------------------------------- **/
+				inject(function(zpzMdCalendarUtilitySvc) {
+					var src = { e1: { a1: 5 } },
+						out = zpzMdCalendarUtilitySvc.mergeRecursive(dst, src);
+					out.e1.a1.should.equal(5);
+				})
+			);
+		}
+	)
 });
